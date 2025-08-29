ce5e215967f91b79e8e91b2054a905b1c9f28fbb
Yerosh <adilbekernur@gmail.com>
2025-08-29 20:28:12 +0500

    Initial Commit

---
+++ SwiftUIThinkingBootcamp/SwiftUIThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThinkingBootcampApp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 29.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
