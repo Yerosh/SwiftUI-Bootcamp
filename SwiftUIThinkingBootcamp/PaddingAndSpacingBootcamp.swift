788b4b71f904b5ee85f742f813863a2a34307a56
Yerosh <adilbekernur@gmail.com>
2025-09-05 23:14:47 +0500

    Learning Padding

---
+++ SwiftUIThinkingBootcamp/PaddingAndSpacingBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  PaddingAndSpacingBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 05.09.2025.
+//
+
+import SwiftUI
+
+struct PaddingAndSpacingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaddingAndSpacingBootcamp()
+}
