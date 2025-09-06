afa1a6a2cb1bc75522bea5c2d9ce7b7b36d4b7c4
Yerosh <adilbekernur@gmail.com>
2025-09-06 23:03:52 +0500

    Learning Padding

--- SwiftUIThinkingBootcamp/PaddingAndSpacingBootcamp.swift
+++ SwiftUIThinkingBootcamp/PaddingAndSpacingBootcamp.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct PaddingAndSpacingBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 20)
+            
+            Text("This is second line of the text. As a result, there will be a 20 point spacing between the two lines")
+        }
+        .padding()
+        .padding(.vertical, 10)
+        .background(
+            Color.white
+                .cornerRadius(10)
+                .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 10)
+        )
+        .padding(.horizontal, 10)
     }
 }
 
