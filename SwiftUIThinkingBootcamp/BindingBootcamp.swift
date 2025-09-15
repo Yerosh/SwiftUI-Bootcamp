590f8c3cc0f062778304cec0621472f69d65101c
Yerosh <adilbekernur@gmail.com>
2025-09-15 21:06:38 +0500

    Learning Bindings

---
+++ SwiftUIThinkingBootcamp/BindingBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  BindingBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 15.09.2025.
+//
+
+import SwiftUI
+
+struct BindingBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BindingBootcamp()
+}
