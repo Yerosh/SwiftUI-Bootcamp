13bd55cf88713aef34e75b9febd2fa11689c9881
Yerosh <adilbekernur@gmail.com>
2025-09-16 19:48:45 +0500

    Learning Bindings

--- SwiftUIThinkingBootcamp/BindingBootcamp.swift
+++ SwiftUIThinkingBootcamp/BindingBootcamp.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct BindingBootcamp: View {
+    @State var backgroundColor: Color = Color.green
+    @State var title: String = "Title"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            backgroundColor.ignoresSafeArea(edges: .all)
+            
+            VStack {
+                Text(title)
+                    .foregroundColor(.white)
+                
+                ButtonView(backgroundColor: $backgroundColor, title: $title)
+            }
+        }
+    }
+}
+
+
+struct ButtonView: View {
+    
+    @Binding var backgroundColor: Color
+    @State var buttonColor: Color = .blue
+    @Binding var title: String
+    
+    var body: some View {
+        Button {
+            backgroundColor = Color.orange
+            buttonColor = .pink
+            title = "Pressed!"
+        } label: {
+            Text("Button")
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(buttonColor)
+                .cornerRadius(10)
+            
+        }
+
     }
 }
 
