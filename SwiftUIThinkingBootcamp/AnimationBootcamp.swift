0c8a318d2aae4c7b2ac334106a018756314056ed
Yerosh <adilbekernur@gmail.com>
2025-09-17 23:54:16 +0500

    Learning Animations

---
+++ SwiftUIThinkingBootcamp/AnimationBootcamp.swift
@@ -0,0 +1,37 @@
+//
+//  AnimationBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 17.09.2025.
+//
+
+import SwiftUI
+
+struct AnimationBootcamp: View {
+    
+    @State var isAnimated: Bool = false
+    
+    var body: some View {
+        VStack {
+            Button("Button") {
+                    isAnimated.toggle()
+                
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: isAnimated ? 50 : 25)
+                .fill(isAnimated ? Color.red : Color.green)
+                .frame(
+                    width: isAnimated ? 100 : 300,
+                    height: isAnimated ? 100 : 300
+                )
+                .rotationEffect(Angle(degrees: isAnimated ? 360 : 0))
+                .offset(y: isAnimated ? 300 : 0)
+                .animation(.default)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    AnimationBootcamp()
+}
