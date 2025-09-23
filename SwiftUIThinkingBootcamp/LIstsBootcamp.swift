c47aadf83c912b1e3f2a5118c9d157a0430fd11a
Yerosh <adilbekernur@gmail.com>
2025-09-23 20:52:51 +0500

    Learning Lists

---
+++ SwiftUIThinkingBootcamp/LIstsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  LIstsBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 23.09.2025.
+//
+
+import SwiftUI
+
+struct LIstsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LIstsBootcamp()
+}
