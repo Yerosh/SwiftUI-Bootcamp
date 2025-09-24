8ddef01ae2401e78750c3ea67da7e786d7f48ab8
Yerosh <adilbekernur@gmail.com>
2025-09-24 23:50:53 +0500

    Continue Lists

--- SwiftUIThinkingBootcamp/ListsBootcamp.swift
+++ SwiftUIThinkingBootcamp/ListsBootcamp.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct LIstsBootcamp: View {
+struct ListsBootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    LIstsBootcamp()
+    ListsBootcamp()
 }
