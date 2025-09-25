78027d9a3c27ee0fb4a3864f470106bcd5e92fc5
Yerosh <adilbekernur@gmail.com>
2025-09-25 23:17:01 +0500

    Continue Lists

--- SwiftUIThinkingBootcamp/ListsBootcamp.swift
+++ SwiftUIThinkingBootcamp/ListsBootcamp.swift
@@ -8,8 +8,67 @@
 import SwiftUI
 
 struct ListsBootcamp: View {
+    
+    @State var fruits: [String] = [
+        "Apple",
+        "Banana",
+        "Orange",
+        "Mango"
+    ]
+    
+    @State var veggies: [String] = [
+        "Carrot",
+        "Broccoli",
+        "Spinach",
+        "Beetroot"
+    ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section(
+                    header:
+                        HStack {
+                            Text("Fruits")
+                            Image(systemName: "flame.fill")
+                        }
+                        .font(.headline)
+                        .foregroundColor(.orange)
+                ) {
+                        ForEach(fruits, id: \.self) { fruit in
+                            Text(fruit)
+                        }
+                        .onDelete(perform: delete)
+                        .onMove(perform: move)
+                    }
+                Section(header: Text("Veggies")) {
+                        ForEach(veggies, id: \.self) { veggie in
+                            Text(veggie)
+                        }
+                    }
+            }
+            .navigationTitle("Grocery List")
+            .navigationBarItems(leading: EditButton(), trailing: addButton)
+        }
+        .accentColor(.red)
+    }
+    
+    var addButton: some View {
+        Button("Add") {
+            add()
+        }
+    }
+    
+    func delete(at offsets: IndexSet) {
+        fruits.remove(atOffsets: offsets)
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        fruits.move(fromOffsets: source, toOffset: destination)
+    }
+    
+    func add() {
+        fruits.append("newItem")
     }
 }
 
