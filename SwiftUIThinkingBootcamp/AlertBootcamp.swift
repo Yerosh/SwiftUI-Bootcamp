d4b4cf9968f28f27358532911153562497d423a9
Yerosh <adilbekernur@gmail.com>
2025-09-26 20:54:00 +0500

    Learning Alerts

---
+++ SwiftUIThinkingBootcamp/AlertBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AlertBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 26.09.2025.
+//
+
+import SwiftUI
+
+struct AlertBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertBootcamp()
+}
