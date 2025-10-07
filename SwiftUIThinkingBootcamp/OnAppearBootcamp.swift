324a77778c5b295151c48daf8a44d6508feeb2c9
Yerosh <adilbekernur@gmail.com>
2025-10-07 23:31:12 +0500

    Learning OnAppear

---
+++ SwiftUIThinkingBootcamp/OnAppearBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  OnAppearBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 07.10.2025.
+//
+
+import SwiftUI
+
+struct OnAppearBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnAppearBootcamp()
+}
