e451d243cc3149083c8a57b7883ab08269523a42
Yerosh <adilbekernur@gmail.com>
2025-10-09 23:30:41 +0500

    Learning If Let Guard

---
+++ SwiftUIThinkingBootcamp/IfLetGuardBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  IfLetGuardBootcamp.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 09.10.2025.
+//
+
+import SwiftUI
+
+struct IfLetGuardBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IfLetGuardBootcamp()
+}
