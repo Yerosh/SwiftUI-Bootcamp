20cc3243e230b7fd79a5ca16a9cbda6d75bec520
Yerosh <adilbekernur@gmail.com>
2025-10-10 18:07:29 +0500

    Continue If Let Guard

--- SwiftUIThinkingBootcamp/IfLetGuardBootcamp.swift
+++ SwiftUIThinkingBootcamp/IfLetGuardBootcamp.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct IfLetGuardBootcamp: View {
+    
+    @State var currentUserID: String? = nil
+    @State var displayText: String? = nil
+    @State var isLoading: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("Here we are practicing safe coding!")
+                if let text = displayText {
+                    Text(text)
+                        .font(.title)
+                }
+                
+                if isLoading {
+                    ProgressView()
+                }
+                
+                Spacer()
+            }
+            .navigationTitle("Safe Coding")
+            .onAppear {
+                loadData()
+            }
+        }
+    }
+    
+    
+    func loadData() {
+        if let userID = currentUserID {
+            isLoading = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                displayText = "This is the new data! User id is - \(userID)"
+                isLoading = false
+            }
+        } else {
+            displayText = "No user id found!"
+        }
+    }
+    
+    
+    func loadData2() {
+        guard let userID = currentUserID else {
+            displayText = "No user id found!"
+            return
+        }
+        
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.displayText = "This is the new data! User id is - \(userID)"
+            self.isLoading = false
+        }
     }
 }
 
