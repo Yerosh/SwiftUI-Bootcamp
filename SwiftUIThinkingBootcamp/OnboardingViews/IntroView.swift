71c3b1a5ce0f6f41c646439192ea34c39d32b8c7
Yerosh <adilbekernur@gmail.com>
2025-10-14 23:54:45 +0500

    Learning Onboarding Views

---
+++ SwiftUIThinkingBootcamp/OnboardingViews/IntroView.swift
@@ -0,0 +1,34 @@
+//
+//  IntroView.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 14.10.2025.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    
+    @AppStorage("signed_in") var currentUserSignedIn: Bool = false
+    
+    var body: some View {
+        ZStack {
+            RadialGradient(
+                gradient: Gradient(colors: [Color.pink, Color.blue]),
+                center: .topLeading,
+                startRadius: 5,
+                endRadius: UIScreen.main.bounds.height)
+            .ignoresSafeArea()
+            
+            if currentUserSignedIn {
+                Text("Profile View")
+            } else {
+                OnboardingView()
+            }
+        }
+    }
+}
+
+#Preview {
+    IntroView()
+}
---
+++ SwiftUIThinkingBootcamp/OnboardingViews/OnboardingView.swift
@@ -0,0 +1,225 @@
+//
+//  OndoardingView.swift
+//  SwiftUIThinkingBootcamp
+//
+//  Created by Yernur Adilbek on 14.10.2025.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    // Onboarding states:
+    /*
+     0 - Welcome screen
+     1 - Add name
+     2 - Add age
+     3 - Add gender
+     */
+    @State var onboardingState: Int = 0
+    let transition: AnyTransition = .asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading))
+    
+    
+    @State var name: String = ""
+    @State var age: Double = 50
+    @State var gender: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
+    
+    var body: some View {
+        ZStack {
+            //content
+            ZStack {
+                switch onboardingState {
+                case 0:
+                    welcomeSection
+                        .transition(transition)
+                case 1:
+                    addNameSection
+                        .transition(transition)
+                case 2:
+                    addAgeSection
+                        .transition(transition)
+                case 3:
+                    addGenderSection
+                        .transition(transition)
+                default:
+                    RoundedRectangle(cornerRadius: 25)
+                        .foregroundColor(.green)
+                }
+            }
+            
+            //buttons
+            VStack {
+                Spacer()
+                bottomButton
+            }
+            .padding(30)
+        }
+        .alert(isPresented: $showAlert, content: {
+            return Alert(title: Text(alertTitle))
+        })
+    }
+}
+
+#Preview {
+    OnboardingView()
+        .background(.purple)
+}
+
+// MARK: COMPONENTS
+
+extension OnboardingView {
+    private var bottomButton: some View {
+        Text(onboardingState == 0 ? "Sign up".uppercased() :
+                onboardingState == 3 ? "Finish".uppercased() : "Next".uppercased())
+            .font(.headline)
+            .foregroundColor(.purple)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(Color.white)
+            .cornerRadius(10)
+            .animation(nil, value: onboardingState)
+            .onTapGesture {
+                handleNextButtonPressed()
+            }
+    }
+    
+    private var welcomeSection: some View {
+        VStack(spacing: 40) {
+            Spacer()
+            Image(systemName: "heart.text.square.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .foregroundColor(.white)
+            
+            Text("Find your match.")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .fontWeight(.semibold)
+                .overlay(
+                    Capsule(style: .continuous)
+                        .frame(height: 3)
+                        .offset(y: 5)
+                        .foregroundColor(.white)
+                    , alignment: .bottom
+                )
+                Text("This is the #1 app for finding your match online! In this tutorial we are practicing using AppStorage and other SwiftUI techniques.")
+                .fontWeight(.medium)
+                .foregroundColor(.white)
+            
+            Spacer()
+            Spacer()
+        }
+        .multilineTextAlignment(.center)
+        .padding(30)
+    }
+    
+    private var addNameSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            
+            Text("What's your name?")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .fontWeight(.semibold)
+            TextField("Your name here...", text: $name)
+                .font(.headline)
+                .frame(height: 55)
+                .padding(.horizontal)
+                .background(.white)
+                .cornerRadius(10)
+            Spacer()
+            Spacer()
+        }
+        .padding(30)
+    }
+    
+    private var addAgeSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            
+            Text("What's your age?")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .fontWeight(.semibold)
+            Text("\(String(format: "%.0f", age))")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+            Slider(value: $age, in: 18...100, step: 1)
+                .accentColor(.white)
+            Spacer()
+            Spacer()
+        }
+        .padding(30)
+    }
+    
+    private var addGenderSection: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            
+            Text("What's your gender?")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .fontWeight(.semibold)
+            
+            Menu {
+                Picker(selection: $gender, label: EmptyView()) {
+                    Text("Male").tag("Male")
+                    Text("Female").tag("Female")
+                    Text("Non-Binary").tag("Non-Binary")
+                }
+            } label: {
+                Text(gender.isEmpty ? "Select a gender" : gender)
+                    .font(.headline)
+                    .foregroundColor(.purple)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 2)
+            }
+
+            
+            
+            Spacer()
+            Spacer()
+        }
+        .padding(30)
+    }
+}
+
+// MARK: FUNCTIONS
+
+extension OnboardingView {
+    func handleNextButtonPressed() {
+        
+        // CHECK INPUTS
+        switch onboardingState {
+            case 1:
+            guard name.count >= 3 else {
+                showAlert(title: "Your name must be at least 3 characters long!😞")
+                return
+            }
+            default:
+                break
+        }
+        
+        // GO TO NEXT SECTION
+        if onboardingState == 3 {
+            // sign in
+        } else {
+            withAnimation(.spring()) {
+                onboardingState += 1
+            }
+        }
+    }
+    
+    func showAlert(title: String) {
+        alertTitle = title
+        showAlert.toggle()
+    }
+}
